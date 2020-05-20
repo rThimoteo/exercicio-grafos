ece04f0e3b7e71100ef450f4883f9d847f4af9cc
Rodrigo Thimoteo <53916300+rThimoteo@users.noreply.github.com>
2020-05-20 20:51:32 -0300

    Update Algoritmos.java

--- src/com/algoritmos/Algoritmos.java
+++ src/com/algoritmos/Algoritmos.java
@@ -16,15 +16,17 @@ public class Algoritmos {
 		
 		Aresta arestaAux;
 		Vertice verticeAux1, verticeAux2;
-		int opcao = 5, peso;
+		int opcao = 5;
+		double peso = 0;
 		String origem , destino;
 		
 		while(opcao!=0){
-			System.out.println("1 - Add vertices e aresta");
-			System.out.println("2 - Imprimir Grafo dado");
-			System.out.println("3 - Obter arvore de Busca em Profundidade");
-			System.out.println("4 - Obter arvore de Busca em Largura");
-			System.out.println("0 - fim"+"\n");
+			System.out.println("1 - Criar Grafo");
+			System.out.println("2 - Add Aresta");
+			System.out.println("3 - Imprimir Grafo dado");
+			System.out.println("4 - Obter arvore de Busca em Profundidade");
+			System.out.println("5 - Obter arvore de Busca em Largura");
+			System.out.println("0 - fim");
 			
 			opcao = Keyboard.readInt();
 			
@@ -37,8 +39,54 @@ public class Algoritmos {
 			
 			switch (opcao){
 			case 1:
-				System.out.print("Coloque o peso: "+"\n");
-				peso = Keyboard.readInt();
+				System.out.print("Escolha o Numero de colunas (entre 2 e 6): \n");
+				int op = 0;
+				op = Keyboard.readInt();
+				if (op<2 || op>6) {
+					System.out.print("\nO Numero deve estar entre 2 e 6");
+					System.out.print("\n\nENTER para continuar");
+					Keyboard.readInt();
+					break;
+				}
+				
+				if(op>1) {
+					inicial.addVertice("a");
+					inicial.addVertice("b");
+					inicial.addVertice("c");
+					inicial.addVertice("d");
+					inicial.addVertice("e");
+					inicial.addVertice("f");
+				}
+				if(op>2) {
+					inicial.addVertice("g");
+					inicial.addVertice("h");
+					inicial.addVertice("i");
+				}
+				if(op>3) {	
+					inicial.addVertice("j");
+					inicial.addVertice("k");
+					inicial.addVertice("l");
+				}
+				if(op>4) {
+					inicial.addVertice("m");
+					inicial.addVertice("n");
+					inicial.addVertice("o");
+				}
+				if(op>5){
+					inicial.addVertice("p");
+					inicial.addVertice("q");
+					inicial.addVertice("r");
+				}
+				inicial.imprimeArvore();
+			break;
+			case 2:	
+				System.out.print("Coloque o peso (1=1, 2=Raiz de 2): "+"\n");
+				int tipoP = 0;
+				tipoP = Keyboard.readInt();
+				if (tipoP==1)
+					peso = 1;
+				if (tipoP==2)
+					peso=Math.sqrt(2);
 				System.out.print("Coloque o origem: "+"\n");
 				origem = Keyboard.readString();
 				System.out.print("Coloque o Destino: "+"\n");
@@ -47,11 +95,11 @@ public class Algoritmos {
 				
 				inicial.imprimeArvore();
 				break;
-			case 2:
+			case 3:
 				inicial.imprimeArvore();
 				break;
 				
-			case 3:
+			case 4:
 				//Algoritmo de Busca em Profundidade
 				
 				origem = Keyboard.readString();
@@ -60,7 +108,7 @@ public class Algoritmos {
 				
 				resultado.imprimeArvore();
 				break;
-			case 4:
+			case 5:
 				//Algoritmo de Busca em Largura
 				
 				origem = Keyboard.readString();
